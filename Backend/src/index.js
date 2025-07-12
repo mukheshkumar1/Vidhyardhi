387271bab5aa2cdbd4bf95da756240ed26cc8680
mukheshkumar1 <86228784+mukheshkumar1@users.noreply.github.com>
2025-07-12 20:26:55 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://vidhyardhi.vercel.app/',
+    origin: 'https://vidhyardhi.vercel.app',
     credentials: true,
   }));
 
