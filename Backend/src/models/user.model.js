4ae15ec79c7892b88ff8f53f05fd3cb0812ad287
mukheshkumar1 <86228784+mukheshkumar1@users.noreply.github.com>
2025-07-19 11:44:58 +0530

    Update user.model.js

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -53,15 +53,24 @@ const staffSchema= new mongoose.Schema({
         type: String,
         required: true,
       },
-     attendance: {
-    daily: { type: Map, of: String }, 
-    monthly: { type: Map, of: Object },
-    yearly: {
-      workingDays: Number,
-      presentDays: Number,
-      percentage: Number,
-    },
-  },
+      attendance: {
+        daily: {
+          type: Map,
+          of: String,
+          default: new Map(),
+        },
+        monthly: {
+          type: Map,
+          of: Object,
+          default: new Map(),
+        },
+        yearly: {
+          workingDays: { type: Number, default: 0 },
+          presentDays: { type: Number, default: 0 },
+          percentage: { type: Number, default: 0 },
+        },
+      },
+      
       className: {
         type: String,
         required: function () {
